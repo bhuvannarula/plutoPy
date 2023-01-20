00e728ac774b403ede3b39488abb070310f521d7
Bhuvan Narula <55291162+bhuvannarula@users.noreply.github.com>
2023-01-21 03:59:48 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='plutoPy',
+    url='https://github.com/bhuvannarula/plutoPy',
+    author='Inter IIT',
+    author_email='bhuvannarula8@gmail.com',
+    packages=['plutopy'],
+    package_data={
+        '': ['*.py']
+    },
+    install_requires=[],
+    version='1.0',
+    license='MIT',
+    description='A Python Wrapper for Controlling Pluto Drone',
+    #long_description=open('README.md').read(),
+)
